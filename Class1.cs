8f4a2ee9cfc44183a1ad97f927e824b1bd3273c0
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-22 11:34:13 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+public interface IData
+{
+    public string GetData();//1
+}
+
+public interface ICard
+{
+    public string GetCard();//2
+}
+
+public interface IName : IData, ICard
+{
+    public string GetName();//3
+}
+
+public class Person : IName, ICard
+{
+    public string GetName()
+    {
+        return "Name";
+    }
+    public string GetCard()
+    {
+        return "Card";
+    }
+    public string GetData()
+    {
+        return "Data";
+    }
+}
+public class Student : Person, ICard
+{
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Class_0522_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();//一般建立物件
+
+            Console.WriteLine(p1.GetData());
+
+            Console.WriteLine(p1.GetName());
+
+            Console.WriteLine(p1.GetCard());
+           
+            Student s1 =new Student();
+            Console.WriteLine(s1.GetCard());
+            Console.WriteLine(s1.GetName());
+            Console.WriteLine(s1.GetData());
+        }
+    }
+}
